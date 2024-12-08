b90146e6362b8636d560a5e396b9e397aacebfe2
pritam1322 <pritamjat98@gmail.com>
2024-12-08 21:41:41 +0530

    stripe setup 1

--- src/app/(jobapplication)/createJobApplication/page.tsx
+++ src/app/(jobapplication)/createJobApplication/page.tsx
@@ -71,7 +71,7 @@ export default function CreateJobApplication() {
   };
 
   return (
-    <div className="min-h-screen mt-12 flex justify-center items-center">
+    <div className="min-h-screen mt-12 flex flex-col justify-center items-center">
       <div className="bg-neutral-900 p-8 rounded-lg shadow-md max-w-lg w-full">
         <h1 className="text-3xl font-bold text-white mb-6 text-center">
           Create New Job Application
