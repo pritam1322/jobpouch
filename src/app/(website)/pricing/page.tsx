f4d1f96c9b5074f3744f96ccd6f728b0c7d72eec
pritam1322 <pritamjat98@gmail.com>
2024-12-27 23:45:11 +0530

    error fix 3

--- src/app/(website)/pricing/page.tsx
+++ src/app/(website)/pricing/page.tsx
@@ -39,7 +39,7 @@
 
   declare global {
     interface Window {
-      Razorpay: new (options: RazorpayOptions) => { open: () => void; [key: string]: any };
+      Razorpay: new (options: RazorpayOptions) => { open: () => void; };
     }
   }
 
